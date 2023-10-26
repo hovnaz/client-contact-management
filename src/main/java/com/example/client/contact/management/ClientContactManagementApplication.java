0a5be32519b77dc433809382430acd2540931efe
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2023-10-26 16:31:37 +0400

    Initial commit

---
+++ src/main/java/com/example/client/contact/management/ClientContactManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.client.contact.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientContactManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientContactManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/client/contact/management/ClientContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client.contact.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContactManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
